5d95de082521c863115538ed58f6593930c290c8
Kevin Reyes <kev.andrew.reyes@gmail.com>
2020-04-10 11:31:41 -0700

    changed connection config

--- config/connection.js
+++ config/connection.js
@@ -3,17 +3,15 @@ var mysql = require("mysql");
 
 var connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Ranger2018!",
-    database: "burger_db"
-  });
+
+connection = mysql.createConnection(process.env.JAWSDB_URL || {
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Ranger2018!",
+  database: "burger_db"
 });
+
 
 // Make connection.
 connection.connect(function (err) {
